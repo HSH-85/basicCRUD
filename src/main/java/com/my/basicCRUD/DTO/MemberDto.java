307c6b259a8cfc7fea398453f5bf954a84af336d
HSH-85 <gjtjdgns@gmail.com>
2025-02-18 14:59:23 +0900

    # 3. Validation 처리

--- src/main/java/com/my/basicCRUD/DTO/MemberDto.java
+++ src/main/java/com/my/basicCRUD/DTO/MemberDto.java
@@ -1,24 +1,26 @@
 package com.my.basicCRUD.DTO;
 
 import com.my.basicCRUD.entitiy.Member;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 public class MemberDto {
     private Long id;
+    @Size(min = 2, message = "이름은 2자 이상으로 입력해야 합니다.")
     private String name;
+    @Range(min = 0, max = 120, message = "나이는 0세부터 120세까지입니다.")
     private int age;
+
+    @NotBlank(message = "주소는 꼭 입력해야 합니다.")
     private String address;
 
-    public MemberDto(Long id, String name, int age, String address) {
-        this.id = id;
-        this.name = name;
-        this.age = age;
-        this.address = address;
-    }
     // Entity -> DTO로 변환
     public static MemberDto fromEntity(Member entity) {
         return new MemberDto(
